5d204b790af8684852c3dd9745ca95bf75d22b56
faisal2363 <fn237333@gmail.com>
2022-04-14 21:39:11 +0500

    adding containers

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
   @override
   _InputPageState createState() => _InputPageState();
 }
@@ -12,15 +14,58 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              ),
+              Expanded(child: Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+    color: Color(0xFF1D1E33),
+    borderRadius: BorderRadius.circular(10.0),
+    ),
+    ),
+              )
 
-        },
-        child: Icon(Icons.add),
+            ],
+          ),),
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1D1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),),
+          Expanded(child: Row(
+          children: <Widget>[
+    Expanded(child: Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
       ),
+    ),),
+    Expanded(child: Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),),
+
+    ],
+    ),),
+
+        ],
+      )
     );
   }
 }
