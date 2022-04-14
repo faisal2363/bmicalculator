6b1d724d9af6f545ea2c1149de5b35d1944e5747
faisal2363 <fn237333@gmail.com>
2022-04-14 21:41:06 +0500

    adding const

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,24 +12,24 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: const Text('BMI CALCULATOR'),
       ),
       body: Column(
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
               Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
+                margin: const EdgeInsets.all(15.0),
                 decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
+                  color: const Color(0xFF1D1E33),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
               ),
               Expanded(child: Container(
-    margin: EdgeInsets.all(15.0),
+    margin: const EdgeInsets.all(15.0),
     decoration: BoxDecoration(
-    color: Color(0xFF1D1E33),
+    color: const Color(0xFF1D1E33),
     borderRadius: BorderRadius.circular(10.0),
     ),
     ),
@@ -38,25 +38,25 @@ class _InputPageState extends State<InputPage> {
             ],
           ),),
           Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
+            margin: const EdgeInsets.all(15.0),
             decoration: BoxDecoration(
-              color: Color(0xFF1D1E33),
+              color: const Color(0xFF1D1E33),
               borderRadius: BorderRadius.circular(10.0),
             ),
           ),),
           Expanded(child: Row(
           children: <Widget>[
     Expanded(child: Container(
-      margin: EdgeInsets.all(15.0),
+      margin: const EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: const Color(0xFF1D1E33),
         borderRadius: BorderRadius.circular(10.0),
       ),
     ),),
     Expanded(child: Container(
-      margin: EdgeInsets.all(15.0),
+      margin: const EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: const Color(0xFF1D1E33),
         borderRadius: BorderRadius.circular(10.0),
       ),
     ),),
